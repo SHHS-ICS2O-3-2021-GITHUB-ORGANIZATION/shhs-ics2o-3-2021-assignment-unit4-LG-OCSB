d449109b0d6686428201c993a42521adc6a4f706
Lucas Galan <97174691+LG-OCSB@users.noreply.github.com>
2022-01-24 13:09:40 +0000

    Top half has been added

--- UNIT 4/Code/main.py
+++ UNIT 4/Code/main.py
@@ -1,12 +1,12 @@
-# NAME OF AUTHOR:  
-# NAME OF THE PROGRAM:  
-# DATE OF CREATION:  
-# PURPOSE OF PROGRAM:  
+# NAME OF AUTHOR: Lucas Galan
+# NAME OF THE PROGRAM:  Q3 Taxes
+# DATE OF CREATION: Monday January 24 2022 
+# PURPOSE OF PROGRAM: Calculate the costs of items including Taxes
 
 
 # VARIABLE DEFINITION
 
-lucas
+
 
 
 
