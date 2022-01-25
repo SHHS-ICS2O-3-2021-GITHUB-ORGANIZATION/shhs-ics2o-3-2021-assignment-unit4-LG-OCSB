108c51fd1a2d9af8335e0690402f2f4cba6fd78b
Lucas Galan <97174691+LG-OCSB@users.noreply.github.com>
2022-01-25 14:10:02 +0000

    Full version of Q1AgeChecker

---
+++ UNIT 4/All of unit 4/Q1Agechecker.py
@@ -0,0 +1,56 @@
+# NAME OF AUTHOR:  Lucas Galan
+# NAME OF THE PROGRAM:  Q1AgeChecker
+# DATE OF CREATION:  January 25th, 2022
+# PURPOSE OF PROGRAM:  Find out how smart you are
+
+
+# VARIABLE DEFINITION
+#userAge=int(input())
+
+
+
+# INPUT
+print("Please enter your age :")
+userAge= int(input())
+
+
+# PROCESSING
+if userAge <=(4):
+ print("you are not in school yet!")
+  ########################################## if young then you cant be in school
+if userAge == (5):
+ print("You must be in elemtary school!")
+if userAge == (6):
+ print("You must be in elemtary school!")
+if userAge == (7):
+ print("You must be in elemtary school!")
+if userAge == (8):
+ print("You must be in elemtary school!")
+if userAge == (9):
+ print("You must be in elemtary school!")
+if userAge == (10):
+ print("You must be in elemtary school!")
+if userAge == (11):
+ print("You must be in elemtary school!")
+###############################################if 5-11 then you must be in elementary
+if userAge == (12):
+ print("you must be in intermediate schooling!")
+if userAge == (13):
+ print("you must be in intermediate schooling!")
+if userAge == (14):
+ print("you must be in intermediate schooling!")
+################################################12-14 means you are in middle school
+if userAge == (15):
+ print("you must be in high school!")
+if userAge == (16):
+  print("you must be in high school!")
+if userAge == (17):
+  print("you must be in high school!")
+if userAge == (18):
+  print("you must be in high school!")
+###########################################15-18 is high school
+if userAge >= (19) :
+  print("you must be in collage/university/workforce!")
+#programs determined that the user is in collage/university/workforce because they are 19+
+# OUTPUT
+#The program assumes what level of schooling the user is currently in based on the age they entered.
